be834d2c717681693934b218c3bc239f693e0c66
ffei1 <166067404+ffei1@users.noreply.github.com>
2025-12-13 20:55:14 +0800

    Update init.sh

--- fine3399/kernel-rockchip/init.sh
+++ fine3399/kernel-rockchip/init.sh
@@ -418,13 +418,13 @@ board_set_iface_smp_affinity() {
 		set_iface_cpumask 20 "eth1"
 		;;
 	rumu3f,fine-3399)
-		set_iface_cpumask 10 "eth0" "" 30
-		set_iface_cpumask 20 "eth1" "xhci-hcd:usb[0-9]+" 30
+		set_iface_cpumask 4 "eth0" "" 30
+		set_iface_cpumask 8 "eth1" "xhci-hcd:usb[0-9]+" 30
 		ethtool -K eth1 rx off tx off tso off gso off sg off
 		;;
 	embedfire,lubancat-5)
-		set_iface_cpumask 10 "eth0"
-		set_iface_cpumask 20 "eth1"
+		set_iface_cpumask 4 "eth0"
+		set_iface_cpumask 8 "eth1"
 		;;
 	esac
 }
