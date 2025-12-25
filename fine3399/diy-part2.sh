823df85f793d1e6cce6e4ba8cc775128afc95406
ffei1 <166067404+ffei1@users.noreply.github.com>
2025-12-25 20:45:49 +0800

    Update diy-part2.sh

--- fine3399/diy-part2.sh
+++ fine3399/diy-part2.sh
@@ -9,8 +9,11 @@
 
 
 #移植设备
-# linux/rockchip/image/armv8.mk添加fnet-3399设备型号
+# linux/rockchip/image/armv8.mk添加fine-3399设备型号
 sed -i 's/include legacy\.mk/define Device\/rumu3f_fine-3399\n  DEVICE_VENDOR := RUMU3F\n  DEVICE_MODEL := FINE 3399\n  SOC := rk3399\n  DEVICE_PACKAGES := kmod-usb-net-rtl8152\n  DEVICE_DTS_DIR := \.\.\/dts\n  DEVICE_DTS = rk3399\/rk3399-fine-3399\n  KERNEL = kernel-bin | lzma | fit lzma \$\$(KDIR)\/image-rk3399-fine-3399\.dtb\nendef\nTARGET_DEVICES += rumu3f_fine-3399\n\ninclude legacy\.mk/' target/linux/rockchip/image/armv8.mk
+
+#修改target/linux/rockchip/image/Makefile打包方式
+sed -i 's/dd if="\$(STAGING_DIR_IMAGE)"\/\$(UBOOT_DEVICE_NAME)-u-boot-rockchip\.bin of="\$\@" seek=64 conv=notrunc/dd if="$(STAGING_DIR_IMAGE)"\/$(UBOOT_DEVICE_NAME)-idbloader.img of="$@" seek=64 conv=notrunc\n    dd if="$(STAGING_DIR_IMAGE)"\/$(UBOOT_DEVICE_NAME)-u-boot.itb of="$@" seek=16384 conv=notrunc/' target/linux/rockchip/image/Makefile
 
 # 复制修改好的uboot/Makefile到对应目录
 cp -f $GITHUB_WORKSPACE/fine3399/uboot-rockchip/fine-3399-rk3399_defconfig package/boot/uboot-rockchip/src/configs/fine-3399-rk3399_defconfig
@@ -24,10 +27,6 @@ cp -f $GITHUB_WORKSPACE/fine3399/uboot-rockchip/rk3399-fine-3399-u-boot.dtsi pac
 # 复制patch到对应的目录
 cp -f $GITHUB_WORKSPACE/fine3399/kernel-rockchip/init.sh target/linux/rockchip/armv8/base-files/lib/board/init.sh
 
-cp -f $GITHUB_WORKSPACE/fine3399/kernel-rockchip/Makefile target/linux/rockchip/image/Makefile
-
 cp -f $GITHUB_WORKSPACE/fine3399/kernel-rockchip/02_network target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 cp -f $GITHUB_WORKSPACE/fine3399/kernel-rockchip/rk3399-fine-3399.dts target/linux/rockchip/dts/rk3399/rk3399-fine-3399.dts
-
-
