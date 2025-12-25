4cb7eb7056b55e7c4727ac11aa34ded3547cba6c
ffei1 <166067404+ffei1@users.noreply.github.com>
2025-12-25 16:45:25 +0800

    Update diy-part1.sh

--- fine3399/diy-part1.sh
+++ fine3399/diy-part1.sh
@@ -8,5 +8,6 @@
 #===============================================
 
 # 修改版本为编译日期，数字类型。
-echo 2025120511 > version
+date_version=$(date +"%Y%m%d%H")
+echo $date_version > version
 echo  77d4782035a23e6f19f9c4751451b4e3 > .vermagic
