802075eeb29062da4febb4301db0e6ba3391964f
ffei1 <166067404+ffei1@users.noreply.github.com>
2025-12-25 20:06:21 +0800

    Update diy-part1.sh

--- lubancat-5/diy-part1.sh
+++ lubancat-5/diy-part1.sh
@@ -7,6 +7,19 @@
 # Blog: https://p3terx.com
 #===============================================
 
+Releases_version=$(cat include/version.mk | sed -n 's|.*releases/\([0-9]\+\.[0-9]\+\.[0-9]\+\).*|\1|p')
+http_value=$(wget -qO- "https://mirrors.ustc.edu.cn/openwrt/releases/${Releases_version}/targets/rockchip/armv8/kmods/")
+hash_value=$(echo "$http_value" | sed -n 's/^.*-\([0-9a-f]\{32\}\)\/.*/\1/p' | head -1)
+hash_value=${hash_value:-$(echo "$http_value" | sed -n 's/.*\([0-9a-f]\{32\}\)\/.*/\1/p' | head -1)}
+if [ -n "$hash_value" ] && [[ "$hash_value" =~ ^[0-9a-f]{32}$ ]]; then
+    echo "$hash_value" > .vermagic
+    echo "kernel内核md5校验码：$hash_value"
+fi
+
 # 修改版本为编译日期，数字类型。
-echo 2025120511 > version
-echo 77d4782035a23e6f19f9c4751451b4e3>.vermagic
+date_version=$(date +"%Y%m%d%H")
+echo $date_version > version
+
+# 为iStoreOS固件版本加上编译作者
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version}'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version}\"/g" package/base-files/files/usr/lib/os-release
