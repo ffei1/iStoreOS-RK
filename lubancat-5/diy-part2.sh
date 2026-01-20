d3c2104bfad5393cf7253ac65683b629fd06e8d7
ffei1 <121517219@qq.com>
2026-01-20 21:47:10 +0800

    update

--- lubancat-5/diy-part2.sh
+++ lubancat-5/diy-part2.sh
@@ -47,5 +47,9 @@ cp -f $GITHUB_WORKSPACE/lubancat-5/kernel-rockchip/rk3588-lubancat-5.dts target/
 cp -f $GITHUB_WORKSPACE/lubancat-5/kernel-rockchip/pwmfan target/linux/rockchip/armv8/base-files/etc/init.d/pwmfan
 chmod 755 target/linux/rockchip/armv8/base-files/etc/init.d/pwmfan
 
+mkdir -p target/linux/rockchip/armv8/base-files/usr/sbin
+cp -f $GITHUB_WORKSPACE/lubancat-5/kernel-rockchip/dhcp-watchdog target/linux/rockchip/armv8/base-files/usr/sbin/dhcp-watchdog
+cp -f $GITHUB_WORKSPACE/lubancat-5/kernel-rockchip/02-setup-cron target/linux/rockchip/armv8/base-files/etc/uci-defaults/02-setup-cron
+chmod 755 target/linux/rockchip/armv8/base-files/etc/uci-defaults/02-setup-cron
 #cp -f $GITHUB_WORKSPACE/lubancat-5/kernel-rockchip/init.sh target/linux/rockchip/armv8/base-files/lib/board/init.sh
 #cp -f $GITHUB_WORKSPACE/lubancat-5/kernel-rockchip/02_network target/linux/rockchip/armv8/base-files/etc/board.d/02_network
